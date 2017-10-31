9e92045b1b55ab8cb598d96caed1ec8be696cd2b
Davide <davide.toldo@stud.tu-darmstadt.de>
2017-10-31 21:49:13 +0100

    changed code so that it also tells you how much you have in BCH if you didn't use your BTC from before August 1st on that chain\nput btc balances of different wallets in an array to get the balances separately

--- btc.py
+++ btc.py
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
 import json
 import urllib2
 
-j = urllib2.urlopen('https://api.coindesk.com/v1/bpi/currentprice.json')
-j_obj = json.load(j)
-bpi = j_obj["bpi"]
-eur = bpi["EUR"]
-eurRate = eur["rate"].replace(',','')
+btc_json = urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR')
+btc_obj = json.load(btc_json)
+btcRate = btc_obj[0]["price_eur"]
 
-myCoins = [YOUR NUMBER OF BTC]
+bch_json = urllib2.urlopen('https://api.coinmarketcap.com/v1/ticker/bitcoin-cash/?convert=EUR')
+bch_obj = json.load(bch_json)
+bchRate = bch_obj[0]["price_eur"]
 
-print(float(eurRate)*myCoins)
+btcBeforeBCC = []
+btcAfterBCC = []
+btcAfterSegwit2x = []
+
+print('BTC:')
+for coin in btcAfterBCC:
+    print(str(float(btcRate)*coin)+' €')
+print('BCC:')
+for coin in btcBeforeBCC:
+    print(str(float(bchRate)*coin)+' €')
